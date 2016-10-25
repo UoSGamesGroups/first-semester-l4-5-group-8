87ec4a89ee0a8aa122146ef579e1efcb2a309178
JamesPyke <s178316@ucs.ac.uk>
2016-10-25 20:20:11 +0100

    Re-Tidy

--- Project work/UnityProject/Move.cs
+++ Project work/UnityProject/Move.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class Move : MonoBehaviour {
 	public float speed = 1.0F;
+	public float jump = 1.0F;
 	private Vector3 moveDirection = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		float move = Input.GetAxis ("Horizontal");
+
 		if (Input.GetKeyDown(KeyCode.LeftArrow))
 		{
 			Vector3 position = this.transform.position;
@@ -24,16 +28,9 @@ public class Move : MonoBehaviour {
 			position.x++;
 			this.transform.position = position;
 		}
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			Vector3 position = this.transform.position;
 			position.y++;
-			this.transform.position = position;
-		}
-		if (Input.GetKeyDown(KeyCode.DownArrow))
-		{
-			Vector3 position = this.transform.position;
-			position.y--;
 			this.transform.position = position;
 		}
 	}
