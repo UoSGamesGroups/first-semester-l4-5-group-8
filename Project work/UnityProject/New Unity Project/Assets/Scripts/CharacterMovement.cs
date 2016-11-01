ae3bddd8710ba91fffb9611abd58b9e981f13e40
JamesPyke <s178316@ucs.ac.uk>
2016-11-01 12:36:52 +0000

    Cleaned up project
    CLeaned up code, Added a basic level select

--- Project work/UnityProject/New Unity Project/Assets/Scripts/CharacterMovement.cs
+++ Project work/UnityProject/New Unity Project/Assets/Scripts/CharacterMovement.cs
@@ -4,34 +4,32 @@ using System.Collections;
 public class CharacterMovement : MonoBehaviour
 {
 
-    public float speed, jumpPower;
+    public KeyCode keyLeft, keyRight, keyJump, keyDown;
+
+    public float speed, jumpPower, climbSpeed;
+
+    public bool grounded, climbing;
+
+    private Vector3 defaultScale;
     private Vector2 velocity;
 
-    public LayerMask lyrGround;
+    public LayerMask layerGround;
     public Transform feet;
-
-    public KeyCode keyLeft, keyLeftAlt;
-    public KeyCode keyRight, keyRightAlt;
-    public KeyCode keyJump, keyJumpAlt;
-   
-    public bool grounded;
-
-    Vector3 defaultScale;
 
     private void Start()
     {
         defaultScale = GetComponent<Transform>().localScale;
     }
 
-    private void handleInput()
+    private void UserInput()
     {
-        if (Input.GetKey(keyLeft) || Input.GetKey(keyLeftAlt))
+        if (Input.GetKey(keyLeft))
         {
             velocity.x = -speed;
             GetComponent<Animator>().SetBool("Walking", true);
             GetComponent<Transform>().localScale = new Vector3(-defaultScale.x, defaultScale.y, defaultScale.z);
         }
-        else if (Input.GetKey(keyRight) || Input.GetKey(keyRightAlt))
+        else if (Input.GetKey(keyRight))
         {
             velocity.x = speed;
             GetComponent<Animator>().SetBool("Walking", true);
@@ -44,23 +42,68 @@ public class CharacterMovement : MonoBehaviour
             GetComponent<Animator>().SetBool("Walking", false);
         }
 
-        if ((Input.GetKeyDown(keyJump) || Input.GetKeyDown(keyJumpAlt)) && grounded)
+        
+
+
+        if(climbing)
         {
-            velocity.y = jumpPower;
+            if (Input.GetKey(keyJump))
+            {
+                velocity.y = climbSpeed;
+            }
+            else if(Input.GetKey(keyDown))
+            {
+                velocity.y = -climbSpeed;
+            }
+            else
+            {
+                velocity.y = 0;
+            }
+
         }
+        else
+        {
+            if ((Input.GetKeyDown(keyJump)) && grounded)
+            {
+                velocity.y = jumpPower;
+            }
+        }
+
+
     }
 
     private void Update()
     {
-        grounded = Physics2D.OverlapCircle(feet.position, 0.5f, lyrGround);
+        grounded = Physics2D.OverlapCircle(feet.position, 0.5f, layerGround);
 
-        handleInput();
+        UserInput();
 
         velocity *= Time.deltaTime;
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(velocity.x, GetComponent<Rigidbody2D>().velocity.y + velocity.y);
 
         velocity = Vector2.zero;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ladder")
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+            climbing = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ladder")
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 15;
+            climbing = false;
+        }
     }
 
 }
