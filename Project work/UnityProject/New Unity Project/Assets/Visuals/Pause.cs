70a2595faa19ee70cf49810b695f26d2e245951b
JamesPyke <s178316@ucs.ac.uk>
2016-11-28 21:46:03 +0000

    More refining(don't ask)

---
+++ Project work/UnityProject/New Unity Project/Assets/Visuals/Pause.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+public Transform canvas;
+
+    void update() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else {
+                canvas.gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
