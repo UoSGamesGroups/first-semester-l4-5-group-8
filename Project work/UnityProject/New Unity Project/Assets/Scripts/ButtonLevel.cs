78252bca6891d028a890d97d3167c8fea791d865
JamesPyke <s178316@ucs.ac.uk>
2016-11-28 16:30:20 +0000

    Created new Scenes(Title, controls, exit )

---
+++ Project work/UnityProject/New Unity Project/Assets/Scripts/ButtonLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonLevel : MonoBehaviour {
+
+    public int SceneID;
+
+   public void LoadLevel() {
+        SceneManager.LoadScene(SceneID);
+    }
+
+}
