9248e9ad95887703b4b0391bd7ffe1b37ced44b1
JamesPyke <s178316@ucs.ac.uk>
2016-11-29 11:55:49 +0000

    Added ingame controls and quit

---
+++ Project work/UnityProject/New Unity Project/Assets/Scripts/Pause.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject canvas;
+
+    private bool isPaused;
+
+    public void togglePauseState()
+    {
+        isPaused = !isPaused;
+        canvas.SetActive(isPaused);
+    }
+
+}
